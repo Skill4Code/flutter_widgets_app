0100502cc01a573149bdd0b32ed2146905f5b90a
Jose <lopez.84@outlook.com>
2025-10-30 18:11:15 +0100

    rutas basicas configuradas

--- lib/config/presentations/screens/home/home_screen.dart
+++ lib/config/presentations/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+import 'package:widgets_app/config/presentations/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -58,7 +59,18 @@ class _CustomListTile extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       onTap: () {
-       //TODO: Navegar a la pantalla
+        
+        // Using the Navigator
+        // Navigator.of(context).push(
+        //   MaterialPageRoute<void>(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
+        
+        // Using Named Routes
+        Navigator.pushNamed(context, menuItem.link);
+
+
       },
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/presentations/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/config/presentations/screens/cards/cards_screen.dart';
 import 'package:widgets_app/config/presentations/screens/home/home_screen.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
@@ -14,7 +16,13 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme(slectedColor: 0).getTheme(),
-      home:  const HomeScreen()
+      home:  const HomeScreen(),
+      routes: {
+        '/buttons': (context) => const ButtonsScreen(),
+        '/cards': (context) => const CardsScreen(),
+
+
+      },
     );
   }
 }
